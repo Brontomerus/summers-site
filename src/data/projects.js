60d7db050868dee000f4cb646652ded1a885e052
Brontomerus <bdonelan@revspringinc.com>
2021-05-15 10:16:29 -0400

    some cleanup

--- src/data/projects.js
+++ src/data/projects.js
@@ -1,15 +1,15 @@
 // TODO Add a couple lines about each project
 const data = [
   {
-    title: '...',
-    subtitle: '...',
-    image: '...',
+    title: 'My First Project',
+    subtitle: 'or something',
+    image: '/images/projects/spacepotato.jpg',
     date: '2020-11-20',
     desc:
-      '... '
-      + '... '
-      + '... '
-      + '.',
+      'A project '
+      + 'placeholder '
+      + 'for future use '
+      + 'when I pick my projects and send to brandon.',
   },
 ];
 
